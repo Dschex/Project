f261a0a91e7dd5275b1d025b2871d52d00c019c2
DeeAnna Schexnayder <dschexnayder@clearent.com>
2016-02-12 14:25:10 -0600

    Created MVC solution and made Calculator Face using bootstrap.

---
+++ Calculator/CalculatorMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CalculatorMVC.Startup))]
+namespace CalculatorMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
